00e4719aa3baa8eb197f6b02d1ab282e736a52c0
Yu-Li <yhunsicker@gmail.com>
2022-06-18 00:45:49 -0400

    update to final project

---
+++ index.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+  Particles.init({
+    selector: ".background",
+    sizeVariations: 6,
+    maxParticles: 160,
+    speed: 0.15,
+    color: ["#A9FBC3", "#9AC4F8", "#E38C96", "#F3D77C"],
+    minDistance: 120,
+    connectParticles: false,
+  });
+};
+1;
