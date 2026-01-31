74aa4ef2812e392ca7ef3e85a8165b8d4f8cbece
Yu-Li <45144149+yhunsicker@users.noreply.github.com>
2026-01-31 00:15:08 -0500

    nav & heaader container responsiveness updates. Paused javascript floating dots. Made tweaks with media Queries

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // particles script
-window.onload = function () {
-  Particles.init({
-    selector: ".background",
-    sizeVariations: 6,
-    maxParticles: 160,
-    speed: 0.15,
-    color: ["#A9FBC3", "#9AC4F8", "#E38C96", "#F3D77C"],
-    minDistance: 120,
-    connectParticles: false,
-  });
-};
-1;
+// window.onload = function () {
+//   Particles.init({
+//     selector: ".background",
+//     sizeVariations: 6,
+//     maxParticles: 160,
+//     speed: 0.15,
+//     color: ["#A9FBC3", "#9AC4F8", "#E38C96", "#F3D77C"],
+//     minDistance: 120,
+//     connectParticles: false,
+//   });
+// };
+// 1;
 
 // Lock screen
 // When the user "clicks" on Massachusettes State Government, a modal shows with a message & password.
